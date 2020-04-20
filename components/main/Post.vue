85a2309de43fc73f32a5f848422f49d504bd4253
GoryachkoP <gorpashchenko@gmail.com>
2020-04-20 10:29:02 +0300

    basic template for post

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,18 +1,57 @@
 <template>
-  <article class="post">
+  <el-card shadow="hover" class="post" body-style="padding: 0">
+    <header slot="header" class="post-header">
+      <h3>Post title</h3>
+      <small>
+        <i class="el-icon-date"></i>
+        {{ new Date().toLocaleString() }}</small>
+    </header>
 
-  </article>
+    <div class="post-body">
+      <img
+        src="https://cdn.flixbus.de/2017-09/kiev-header.jpg"
+        alt="post image"
+        class="post-image"
+      />
+    </div>
+
+    <footer class="post-footer">
+      <el-button round @click="openPost">Открыть пост</el-button>
+      <span>
+        <i class="el-icon-message">12</i>
+      </span>
+    </footer>
+  </el-card>
 </template>
 <script>
 export default {
-  
+  methods: {
+    openPost() {
+      console.log("Post opened")
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-  .post {
-    height: 200px;
-    border: 1px solid #000;
-    margin-bottom: 2rem;
-  }
+.post {
+  margin-bottom: 1.5rem;
+}
+.post-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.post-image {
+  width: 100%;
+  height: auto;
+}
+
+.post-footer {
+  padding: 1rem;
+  display: flex;
+  justify-content:space-between;
+  align-items: center;
+}
 </style>
