284ea5643123047601e9c7c2385904f824d783d5
GoryachkoP <gorpashchenko@gmail.com>
2020-05-04 20:13:35 +0300

    list of posts. part 1

--- pages/admin/list.vue
+++ pages/admin/list.vue
@@ -1,12 +1,68 @@
 <template>
-  <div>
-    <h1>Post page</h1>
-  </div>
+  <el-table
+    :data="posts"
+    style="width: 100%"
+  >
+    <el-table-column 
+      prop="title"
+      label="Название"
+    />
+    <el-table-column label="Date">
+      <template slot-scope="{row: {date}}">
+        <i class="el-icon-time"></i>
+        <span style="margin-left: 10px">{{ new Date().toLocaleString() }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column label="Просмотры">
+      <template slot-scope="{row: {views}}">
+        <i class="el-icon-view"></i>
+        <span style="margin-left: 10px">{{ views }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column label="Коментарии">
+      <template slot-scope="{row: {comments}}">
+        <i class="el-icon-chat-line-round"></i>
+        <span style="margin-left: 10px">{{ comments.length }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column label="Действия">
+      <template slot-scope="{row}">
+        <el-button
+          icon="el-icon-edit"
+          type="primary"
+          circle
+          @click="open(row._id)"
+        />
+        </el-button>
+        <el-button
+          icon="el-icon-delete"
+          type="danger"
+          circle
+          @click="remove(row._id)"
+        />
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
 export default {
   layout: 'admin',
-  middleware: ['admin-auth']
+  middleware: ['admin-auth'], 
+  async asyncData({store}) {
+    const posts = await store.dispatch('post/fetchAdmin')
+    return {posts}
+  },
+  methods: {
+    open(id) {
+      console.log('%c%s', 'color: green;', `Open ${id}`)
+    },
+    remove(id) {
+      console.log('%c%s', 'color: red;', `Remove ${id}`)
+    }
+  }
 }
 </script>
