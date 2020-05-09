43110c5744259a17fe743e4740593c34a49ea711
GoryachkoP <gorpashchenko@gmail.com>
2020-05-09 14:50:38 +0300

    post edit

--- pages/admin/post/_id.vue
+++ pages/admin/post/_id.vue
@@ -1,10 +1,51 @@
 <template>
-  <div>
-    <el-breadcrumb separator="/">
+  <div class="page-wrap">
+    <el-breadcrumb separator="/" class="mb">
       <el-breadcrumb-item to="/admin/list">Посты</el-breadcrumb-item>
-      <el-breadcrumb-item>{{ $route.params.id }}</el-breadcrumb-item>
-    </el-breadcrumb>    
-    <span>Post id {{ $route.params.id }}</span>
+      <el-breadcrumb-item>{{ post.title }}</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-form
+      :model="controls"
+      :rules="rules"
+      ref="form"
+      @submit.native.prevent="onSubmit"
+    >
+      <!-- <h3 class="mb">Войти в админпанель</h3> -->
+
+      <el-form-item label="Текст в формате .md или .html" prop="text">
+        <el-input 
+          type="textarea" 
+          v-model.trim="controls.text"
+          resize="none"
+          :rows="10"
+        />
+      </el-form-item>
+
+      <div class="mb">
+        <small class="mr">
+          <i class="el-icon-time"></i>
+          <span>
+            {{ new Date(post.date).toLocaleString() }}
+          </span>
+        </small>
+        <small>
+          <i class="el-icon-view"></i>
+          <span>{{ post.views }}</span>
+        </small>
+      </div>
+
+      <el-form-item class="login-card__button">
+        <el-button 
+          type="primary" 
+          round
+          native-type="submit"
+          :loading="loading"
+        >
+          Обновить
+        </el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -12,9 +53,62 @@
 export default {
   layout: 'admin',
   middleware: ['admin-auth'],
-  mounted() {
-    console.log(this.$route)
+  head() {
+    return {
+      title: `Пост | ${this.post.title}`
+    }
+  },
+  validate({params}) {
+    return !!params.id
+  },
+  async asyncData({store, params}) {
+    const post = await store.dispatch('post/fetchAdminById', params.id)
+    return {post}
+  },
+  data() {
+    return {
+      loading: false,
+      controls: {
+        text: ''
+      },
+      rules: {
+        text: [
+          { required: true, message: 'Текст не должен быть пустым', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.$refs.form.validate(async valid => {
+        if (valid) {
+          this.loading = true
+          const formData = {
+            text: this.controls.text,
+            id: this.post._id
+          }
+          try {
+            await this.$store.dispatch('post/update', formData)
+            this.$message.success('Пост обновлен')
+            this.loading = false
+            this.$router.push('/admin/list/')
+          } catch (e) {
+            this.loading = false
+          }
+        }
+      })
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .page-wrap {
+    width: 600px;
+  }
+  .mr {
+    margin-right: 2rem;
+  }
+</style>
+
 
